8bfd8cd557408efd99412112e58da38bead98789
CMRamirezC <43421100+CMRamirezC@users.noreply.github.com>
2018-12-03 09:19:16 -0800

    Add files via upload

---
+++ 5.3 Radix Sort/Radix Sort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNums;
+            Radix Op = new Radix();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" --> Ejercicio 1 <-- ");
+            arrayNums = new int[] { 3, 6, 9, 5, 1, 4, 7, 2, 1, 3 };
+            Op.Imprimir(arrayNums);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" --> Ejercicio 2 <-- ");
+            arrayNums = new int[] { 8, 3, 9, 3, 11, 7, 1, 27, 12 };
+            Op.Imprimir(arrayNums);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" --> Ejercicio 3 <-- ");
+            arrayNums = new int[] { 10, 40, 36, 5, 24, 2, 5, 8 };
+            Op.Imprimir(arrayNums);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" --> Ejercicio 4 <-- ");
+            arrayNums = new int[] { 55, 42, 0, -3, 0, -1, 2, 4, 7 };
+            Op.Imprimir(arrayNums);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" --> Ejercicio 5 <-- ");
+            arrayNums = new int[] { 25, 108, 1024, 12, 351, 251, 39 };
+            Op.Imprimir(arrayNums);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 5.3 Radix Sort/Radix Sort/Radix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix_Sort
+{
+    class Radix
+    {
+        private void RadixSort(int[] array)
+        {
+            int[] aux = new int[array.Length];//arreglo auxiliar 
+            int r = 2;
+            int b = 32; // variable que representa el numero de bits de un entero
+            int[] count = new int[1 << r];
+            int[] pref = new int[1 << r];
+
+            int groups = (int)Math.Ceiling((double)b / (double)r);
+            int mask = (1 << r) - 1;
+            for (int i = 0, shift = 0; i < groups; i++, shift += r)
+            {
+                for (int j = 0; j < count.Length; j++)
+                    count[j] = 0;
+
+                for (int k = 0; k < array.Length; k++)
+                    count[(array[k] >> shift) & mask]++;
+
+                pref[0] = 0;
+                for (int l = 1; l < count.Length; l++)
+                    pref[l] = pref[l - 1] + count[l - 1];
+
+                for (int m = 0; m < array.Length; m++)
+                    aux[pref[(array[m] >> shift) & mask]++] = array[m];
+
+                aux.CopyTo(array, 0);
+            }
+        }
+        public void Imprimir(int[] array)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" Arreglo de numeros: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" " + array[i] + ",");
+            }
+            RadixSort(array);
+            Console.WriteLine();
+            Console.WriteLine("\n Arreglo de numeros ordenados por Radix: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" " + array[i] + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
